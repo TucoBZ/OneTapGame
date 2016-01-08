6ea86560346fa7992664062e8c8f0b8b398cd9e9
Túlio Bazan da Silva <tucobz@gmail.com>
2016-01-08 11:00:30 -0200

    Blank Project

---
+++ OneTapGameTests/OneTapGameTests.swift
@@ -0,0 +1,36 @@
+//
+//  OneTapGameTests.swift
+//  OneTapGameTests
+//
+//  Created by Túlio Bazan da Silva on 08/01/16.
+//  Copyright © 2016 TulioBZ. All rights reserved.
+//
+
+import XCTest
+@testable import OneTapGame
+
+class OneTapGameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
